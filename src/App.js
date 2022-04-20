5bfb6d913565a2a2f28ad48c5ec367ff519933bb
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-04-20 19:28:49 +0000

    Creating route coin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
 import Coins from './components/Coins'
+import Coin from './routes/Coin'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -21,7 +23,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <Coins coins={coins}/>
+      <Routes>
+        <Route path='/' element={<Coins coins={coins}/>} />
+        <Route path='/coin' element={<Coins/>}>
+          <Route path=':coinId' element={<Coin />}/>
+        </Route>
+      </Routes>
     </>
   );
 }
