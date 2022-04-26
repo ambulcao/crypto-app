2ee15e89e3b284c1946e12efab073285a499e284
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-04-26 21:36:51 +0000

    Creating api coin id

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+
+import './Coin.css'
+import Coins from '../components/Coins'
 
 const Coin = () => {
+
+  const params = useParams()
+  const [coin, setCoin] = useState({})
+
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoin(res.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div>
-      <h1></h1>
+      <div className='coin-container'>
+        <div className='content'>
+          <h1>{coin.name}</h1>
+        </div>
+        <div className='content'>
+          <div className='rank'>
+            <span className='rank-btn'>Rank # {coin.market_cap_rank}</span>
+          </div>
+          <div className='info'>
+            <div className='coin-heading'>
+              {coin.image ? <img src={coin.image.small} alt='' /> : null}
+              <p>{coin.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
