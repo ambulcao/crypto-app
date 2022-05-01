8f091fd74e4b3d6f9b958c86de9b752a8b065beb
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-05-01 13:58:13 +0000

    Verifing mistake api

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import Coins from '../components/Coins'
 
 import './Coin.css'
+import App from '../App'
 
 const Coin = () => {
 
@@ -23,63 +24,8 @@ const Coin = () => {
 
   return (
     <div>
-      <div className='coin-container'>
-        <div className='content'>
-          <h1>{coin.name}</h1>
-        </div>
-        <div className='content'>
-          <div className='rank'>
-            <span className='rank-btn'>Rank # {coin.market_cap_rank}</span>
-          </div>
-          <div className='info'>
-            <div className='coin-heading'>
-              {coin.image ? <img src={coin.image.small} alt='' /> : null}
-              <p>{coin.name}</p>
-              <p>{coin.symbol}</p>
-            </div>
-            <div className='coin-price'>
-              <h1>{coin.market_data.current_price}</h1>
-            </div>
-          </div>
-        </div>
-
-        <div className='content'>
-          <table>
-            <thead>
-              <tr>
-                <th>1h</th>
-                <th>24h</th>
-                <th>7d</th>
-                <th>14d</th>
-                <th>30d</th>
-                <th>1y</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{coin.market_data.price_change_percentage_1h_in_currency.eur}</td>
-                <td>{coin.market_data.price_change_percentage_24h_in_currency.eur}</td>
-                <td>{coin.market_data.price_change_percentage_7d_in_currency.eur}</td>
-                <td>{coin.market_data.price_change_percentage_14d_in_currency.eur}</td>
-                <td>{coin.market_data.price_change_percentage_30d_in_currency.eur}</td>
-                <td>{coin.market_data.price_change_percentage_1y_in_currency.eur}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      
-      <div className='content'>
-        <div className='stats'>
-          <div className='left'>
-
-          </div>
-          <div className='right'>
-            
-          </div>
-        </div>
-      </div>
-      
-      </div>
+      <h1>{coin.name}</h1>
+      <p>{coin.market_cap}</p>
     </div>
   )
 }
